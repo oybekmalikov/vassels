c7b03ba2ec0e16805373067d7ff939020ff50856
oybekmalikov <162029195+oybekmalikov@users.noreply.github.com>
2025-04-10 09:30:49 +0300

    Complated

--- helpers/ErrorHandler.js
+++ helpers/ErrorHandler.js
@@ -1,7 +1,7 @@
 const logger = require("../services/logger.service");
 
 const errorHandler = (error, response) => {
-  // console.log("5555555555555555555555");
+  console.log(error);
   logger.error(error)
   response.status(400).send({ error: error.message });
 };
